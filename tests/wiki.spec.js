be3174a255455a6ed0440b05fd5d4769d1c24db7
Deepakvdk1 <deepakvmech@gmail.com>
2025-07-22 11:10:39 +0530

    wikifile is added

--- tests/wiki.spec.js
+++ tests/wiki.spec.js
@@ -1,7 +1,11 @@
 import { test } from "@playwright/test";
 
+
 test("wikipedia", async ({ page }) => {
-  await page.goto("https://en.wikipedia.org/wiki/Main_Page");
-  await page.getByTitle("Search Wikipedia [alt-shift-f]").fill("world population");
-  await page.screenshot({ path: "example.png" });
+  await page.goto("https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population");
+  let population = await page.locator(`//tr/td/a[@title='Maharashtra']/../../descendant::i`).textContent();
+  function populationFormat(population) {
+    return population.replace(/,/g, '');
+  }
+  console.log(populationFormat(population))
 });
